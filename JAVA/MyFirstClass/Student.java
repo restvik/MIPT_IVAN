538f3504397c5de651e7e1f80ea85494c985ccf8
restvik <restvik@gmail.com>
2014-02-15 00:55:46 +0300

    Create Student.java

---
+++ JAVA/MyFirstClass/Student.java
@@ -0,0 +1,15 @@
+public class Student extends Human {
+	public int mark_m;
+	public int mark_p;
+	
+	public int get_answer(int num_questions){
+		int wr_ans=0;
+		
+		for (int i=0; i < num_questions ; i++)
+				if( (int) (Math.random()*2) == 0){
+					wr_ans+=1;
+				}
+		return wr_ans;
+	}
+
+}
