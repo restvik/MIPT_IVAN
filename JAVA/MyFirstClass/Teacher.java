ee440dd051182a8693afbe8493cd4dc94c8a7975
restvik <restvik@gmail.com>
2014-02-15 01:08:28 +0300

    Update Teacher.java

--- JAVA/MyFirstClass/Teacher.java
+++ JAVA/MyFirstClass/Teacher.java
@@ -1,10 +1,11 @@
 public class Teacher  extends Human {
+	// предмет который преподователь препадает
 	public String subject;
-	
+	// задает вопросы ученику
 	public int  give_questions(){
 		return (int) (Math.random()*5.99);
 	}
-	
+	// ставит ему оценку на основе полученных ответов
 	public int put_mark( int wr_ans){
 		return (5 - wr_ans);
 	}
