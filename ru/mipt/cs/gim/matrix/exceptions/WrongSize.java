52ae1fe3c217ad010c29d5bd0fc46859004bacd6
restvik <restvik@gmail.com>
2014-04-04 20:54:03 +0400

    Create WrongSize.java

---
+++ ru/mipt/cs/gim/matrix/exceptions/WrongSize.java
@@ -0,0 +1,9 @@
+package ru.mipt.cs.gim.matrix.exceptions;
+
+public class WrongSize extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	public WrongSize(String msg) {
+		super (msg);
+	}
+}
